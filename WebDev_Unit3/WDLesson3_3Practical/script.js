f0606da5c8450acedd2ff1494c3570c149297d3e
ibrahimh61 <ibrahimh61@nycstudents.net>
2026-01-07 18:20:07 +0000

    ekjh

--- WebDev_Unit3/WDLesson3_3Practical/script.js
+++ WebDev_Unit3/WDLesson3_3Practical/script.js
@@ -1,1 +1,16 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function w3schools(){
+    document.getElementById("highlight").innerHTML = "w3 schools";
+}
+function tutorial1(){
+    document.getElementById("highlight").innerHTML = "Tutorials Point 1";
+}
+function w3schools2(){
+    document.getElementById("highlight").innerHTML = "w3 schools 2";
+}
+function tutorial2(){
+    document.getElementById("highlight").innerHTML = "Tutorials Point 2";
+}
+function w3school3(){
+    document.getElementById("highlight").innerHTML = "W3 Schools 3";
+}
