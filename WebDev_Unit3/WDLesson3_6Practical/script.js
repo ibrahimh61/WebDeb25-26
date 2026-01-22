816b20b6abed51a5d61aa8737fafbb7711cd9349
ibrahimh61 <ibrahimh61@nycstudents.net>
2026-01-22 02:33:32 +0000

    oiwe

--- WebDev_Unit3/WDLesson3_6Practical/script.js
+++ WebDev_Unit3/WDLesson3_6Practical/script.js
@@ -1,7 +1,13 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
-
+function avg{
+let g1 = parceFloat(document.getElementById("g1")).value;
+let g2 = parceFloat(document.getElementById("g2")).value;
+let g3 = parceFloat(document.getElementById("g2")).value;
+let op = document.getElementById("output");
+let avg = (g1 + g2 + g3)/3;
+op.innerHTML="The average of all 3 grades is ${avg}";
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
-
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
