60fbf665845195f4f3558b9bd267dd691c4333d6
ibrahimh61 <ibrahimh61@nycstudents.net>
2026-01-23 00:59:54 +0000

    jghkj

--- WebDev_Unit3/WDLesson3_6Practical/script.js
+++ WebDev_Unit3/WDLesson3_6Practical/script.js
@@ -1,28 +1,38 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
-function avg{
-let g1 = parceFloat(document.getElementById("g1")).value;
-let g2 = parceFloat(document.getElementById("g2")).value;
-let g3 = parceFloat(document.getElementById("g2")).value;
-let op = document.getElementById("output");
-let avg = (g1 + g2 + g3)/3;
-op.innerHTML="The average of all 3 grades is ${avg}";
+
+function avg(){
+    let g1= parseFloat(document.getElementById("g1").value);
+    let g2= parseFloat(document.getElementById("g2").value);
+    let g3= parseFloat(document.getElementById("g3").value);
+    let op = document.getElementById("output");
+    let avrg = (g1 + g2 + g3)/3;
+    op.innerHTML=`The average of your grades is ${avrg}`;
 }
 
+
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
+
+
 function slope(){
-let x1= parseFloat(document.getElementById("x1").value);
-let y1= parseFloat(document.getElementById("y1").value);
-let x2= parseFloat(document.getElementById("x2").value);
-let y2= parseFloat(document.getElementById("y2").value);
-let op = document.getElementById("op");
-let s = (y2 - y1)/(x2-x1)
-op.innerHTML=`The slope between points (${x1},${y1}) and (${x2},${y2}) is ${s}`;
+    let x1= parseFloat(document.getElementById("x1").value);
+    let y1= parseFloat(document.getElementById("y1").value);
+    let x2= parseFloat(document.getElementById("x2").value);
+    let y2= parseFloat(document.getElementById("y2").value);
+    let op = document.getElementById("op");
+    let s = (y2 - y1)/(x2-x1)
+    op.innerHTML=`The slope between points (${x1},${y1}) and (${x2},${y2}) is ${s}`;
 }
+
+
+
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+
+
+
 function bmi(){
-let h= parseFloat(document.getElementById("h").value);
-let w= parseFloat(document.getElementById("w").value);
-let out = document.getElementById("out");
-let bmi = w/((h)**2)
-out.innerHTML=`Your BMI is ${bmi}`;
+    let h= parseFloat(document.getElementById("h").value);
+    let w= parseFloat(document.getElementById("w").value);
+    let out = document.getElementById("out");
+    let bmi = 703 * w/(h)**2
+    out.innerHTML=`Your BMI is ${bmi}`;
 }
