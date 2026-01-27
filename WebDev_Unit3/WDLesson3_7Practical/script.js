70b6dd375d226eba0f110be4d175415e9eb2bcf8
ibrahimh61 <ibrahimh61@nycstudents.net>
2026-01-27 18:20:27 +0000

    yoygleibdc

--- WebDev_Unit3/WDLesson3_7Practical/script.js
+++ WebDev_Unit3/WDLesson3_7Practical/script.js
@@ -3,7 +3,15 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
-
+function calcDist(){
+      let x1 = parseFloat(document.getElementById("x1").value);
+      let x2 = parseFloat(document.getElementById("x1").value);
+      let y1 = parseFloat(document.getElementById("x1").value);
+      let y2 = parseFloat(document.getElementById("x1").value);
+      let d = Math.sqrt(mathy.pow(x2-x1, 2)+ math.pow(y2-y1, 2));
+      let op = document.getElementById("output")
+      op.innerHtml = 'Dist.between pts (${x1},${x2}) and (${y1},${y2}) is ${d}';
+}
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
       2) Perform the necessary calculations
