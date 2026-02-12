197b300432b6b4a7bf22a74fabf920c60f131de1
ibrahimh61 <ibrahimh61@nycstudents.net>
2026-02-12 03:33:13 +0000

    jdsknfla

--- WebDev_Unit4/WDLesson4_3Practical/script.js
+++ WebDev_Unit4/WDLesson4_3Practical/script.js
@@ -3,6 +3,28 @@
        2) Display BMI status as per the chart in index.html
        3) Display the corresponding image based on the BMI status.
 */
+function calcBMI(){
+       let w = parseFloat(document.getElementById("w").value);
+       let h = parseFloat(document.getElementById("h").value);
+       let output=document.getElementById("output");
+       let bmi= 703 * w/(h)**2;
+       let status="";
+       let filename="";
+       if (bmi<18.5){
+              status="Underweight";
+              filename="underweight.png";
+       }else if (bmi>=18.5 && bmi<=24.9){
+              status="at a Healthy weight";
+              filename="healthyweight.png";
+       }else if (bmi>=25 && bmi <=29.9){
+              status="Overweight";
+              filename="overweight.png";
+       }else{
+              status="Obese";
+              filename="obeseweight.png";
+       }
+       output.innerHTML=`Your BMI is ${bmi}. You are currently ${status}.<br><img src="${filename}">`;
+}
 /* 
 Guideline:
 1) Create the function
